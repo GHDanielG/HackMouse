166d8d89cfcfc6d76a2b46aa1bff0671a9643d36
Daniel <Daniel>
2023-03-05 13:04:15 +0200

    ver4

---
+++ docs/app/immutable/chunks/2-1800bdb3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/page.svelte-8e5db8d7.js";export{t as component};
--- docs/app/immutable/chunks/2-a5996f71.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/page.svelte-01aa067f.js";export{t as component};

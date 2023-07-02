d733ce9a2b518bcf68dbe48bedc4b0b5a81692cf
Daniel <Daniel>
2023-07-03 02:57:19 +0300

    .

--- docs/app/immutable/chunks/2-b5eb0da8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/page.svelte-f7e87438.js";export{t as component};
---
+++ docs/app/immutable/chunks/2-c0ff63b8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/page.svelte-97a36fe4.js";export{t as component};

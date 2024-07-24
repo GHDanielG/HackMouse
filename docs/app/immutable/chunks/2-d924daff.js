92f26127097f5d601236ecf4cf1ccf94262f6beb
Daniel <Daniel>
2024-07-25 02:14:32 +0300

    .

--- docs/app/immutable/chunks/2-d924daff.js
+++ docs/app/immutable/chunks/2-d924daff.js
@@ -1,1 +1,1 @@
-import{default as t}from"../components/pages/_page.svelte-ba591e66.js";export{t as component};
+import{default as t}from"../components/pages/page.svelte-ba591e66.js";export{t as component};
--- docs/app/immutable/chunks/4-ec4e4617.js
+++ docs/app/immutable/chunks/4-ec4e4617.js
@@ -1,1 +1,1 @@
-import{default as t}from"../components/pages/HackMouse/returnCodeDocumantation/_page.svelte-e863e9ee.js";export{t as component};
+import{default as t}from"../components/pages/HackMouse/returnCodeDocumantation/page.svelte-e863e9ee.js";export{t as component};
